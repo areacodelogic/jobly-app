14b67c644a5ee50f6612f2262a071753925cff72
timothy egart <areacodelogic@gmail.com>
2020-06-20 22:33:24 -0700

    Able to create a company and add it the db

--- models/company.js
+++ models/company.js
@@ -63,6 +63,40 @@ class Company {
 
     return company;
   }
+  /** Create a company (from data), update db, return new company data. */
+
+  static async create(data) {
+    const duplicateCheck = await db.query(
+      `SELECT handle 
+        FROM companies
+        WHERE handle = $1`,
+      [data.handle]
+    );
+
+    if (duplicateCheck.rows[0]) {
+      throw new ExpressError(
+        `There already exists a company with handle ${data.handle}`,
+        400
+      );
+    }
+
+    const result = await db.query(
+      `INSERT INTO companies
+          (handle, name, num_employees, description, logo_url)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING handle, name, num_employees, description, logo_url
+        `,
+      [
+        data.handle,
+        data.name,
+        data.num_employees,
+        data.description,
+        data.logo_url,
+      ]
+    );
+
+    return result.rows[0];
+  }
 }
 
 module.exports = Company;
--- routes/companies.js
+++ routes/companies.js
@@ -4,30 +4,37 @@ const Company = require('../models/company');
 
 const router = new express.Router();
 
-
-
 /** GET /  =>  {companies: [company, company]}  */
 
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   try {
     const companies = await Company.findAll(req.query);
-    return res.json({companies});
-  }catch (err){
-      return next(err)
-    }
-  
-})
+    return res.json({ companies });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** GET /[handle]  =>  {company: company} */
 
-router.get('/:handle', async function(req, res, next) {
+router.get('/:handle', async function (req, res, next) {
   try {
     const company = await Company.findOne(req.params.handle);
-    return res.json({company});
-  }catch(err){
-    return next(err)
+    return res.json({ company });
+  } catch (err) {
+    return next(err);
   }
-  
-})
+});
+
+/** POST / {companyData} =>  {company: newCompany} */
+
+router.post('/', async function (req, res, next) {
+  try {
+    const company = await Company.create(req.body);
+    return res.status(201).json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = router;
