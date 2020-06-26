5de08e8605b7e084515ca5a55df34ce1f0a290ee
timothy egart <areacodelogic@gmail.com>
2020-06-25 22:12:29 -0700

    Added Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,58 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Navigation extends Component {
   render() {
-    return <div>Navbar</div>;
+    return (
+      <div>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+          <NavLink className='navbar-brand' to='#'>
+            Jobly
+          </NavLink>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'>
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div
+            className='collapse navbar-collapse justify-content-end'
+            id='navbarNav'>
+            <ul className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='/companies'>
+                  Companies
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='/jobs'>
+                  Jobs
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='/profile'>
+                  Profile
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='/'>
+                  Logout
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='/login'>
+                  Login
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
   }
 }
 
